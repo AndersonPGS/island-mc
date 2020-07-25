c05a8c1f23242e4da2ca79d339d49fc841a18c9b
AndersonPGS <andersonpgs2001@hotmail.com>
2020-07-25 15:06:25 -0300

    quote-title effect writer machine added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
- function btnmore (){
+function typeWriter (elemento) {
+    const textArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 40 * i);
+    });
+}
+
+const title = document.querySelector('.quote-title');
+typeWriter(title);
+
+function btnmore (){
     document.getElementById('sobre').scrollIntoView({behavior: "smooth"});
 }
