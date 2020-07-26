1d150ce7ba579ce9d95601beed9c5d63b5804de3
AndersonPGS <andersonpgs2001@hotmail.com>
2020-07-26 17:45:35 -0300

    effect on btn click hide/show shop or more added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function typeWriter (elemento) {
     const textArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textArray.forEach((letra, i) => {
-        setTimeout(() => elemento.innerHTML += letra, 40 * i);
+        setTimeout(() => elemento.innerHTML += letra, 45 * i);
     });
 }
 
@@ -10,5 +10,21 @@ const title = document.querySelector('.quote-title');
 typeWriter(title);
 
 function btnmore (){
+    var contentSobre = document.querySelector(".content-more");
+    var contentShop = document.querySelector(".content-shop");
+    if (contentSobre.classList.contains("hide")) {
+        contentSobre.classList.remove("hide");
+        contentShop.classList.add("hide");
+    }
     document.getElementById('sobre').scrollIntoView({behavior: "smooth"});
+}
+
+function btnshop (){
+    var contentSobre = document.querySelector(".content-more");
+    var contentShop = document.querySelector(".content-shop");
+    if (contentShop.classList.contains("hide")) {
+        contentSobre.classList.add("hide");
+        contentShop.classList.remove("hide");
+    }
+    //document.getElementById('sobre').scrollIntoView({behavior: "smooth"});
 }
